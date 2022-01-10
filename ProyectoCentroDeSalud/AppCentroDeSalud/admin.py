630b55b16a17319478c397904b2b34f1138a4b69
Alejandro Scribano <scribanoalejandro@gmail.com>
2022-01-10 16:06:47 -0300

    admin habilitado - super usuario creado2

--- ProyectoCentroDeSalud/AppCentroDeSalud/admin.py
+++ ProyectoCentroDeSalud/AppCentroDeSalud/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from AppCentroDeSalud.models import *
 
 # Register your models here.
+
+admin.site.register(CuerpoMedico)
+admin.site.register(Pacientes)
+admin.site.register(Consulta)
--- ProyectoCentroDeSalud/AppCentroDeSalud/models.py
+++ ProyectoCentroDeSalud/AppCentroDeSalud/models.py
@@ -8,6 +8,9 @@ class CuerpoMedico(models.Model):
     dia_hora_atencion = models.CharField(max_length=90)
     email = models.EmailField()
     telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"Nombre y apellido: {self.nombre} {self.apellido} - Especialidad: {self.especialidad} - Día y hora de atención: {self.dia_hora_atencion}"
     
 class Pacientes(models.Model):
     nombre = models.CharField(max_length=30)
@@ -16,9 +19,15 @@ class Pacientes(models.Model):
     email = models.EmailField()
     telefono = models.CharField(max_length=15)
 
+    def __str__(self):
+        return f"Nombre y apellido: {self.nombre} {self.apellido} - Documento: {self.documento} - email: {self.email} - Telefono: {self.telefono}"
+
 class Consulta(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
     telefono = models.CharField(max_length=15)
     consulta_paciente = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"Nombre y apellido: {self.nombre} {self.apellido} - email: {self.email} - Telefono: {self.telefono} - Consulta: {self.consulta_paciente}"
